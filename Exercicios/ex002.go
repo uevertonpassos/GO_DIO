5e6be427b378faac0a2d4133fbc2f109373e91ea
Ueverton Passos <uevertonpassos96@gmail.com>
2023-10-16 19:17:38 -0300

    mais exercicios básicos

--- Exercicios/ex002.go
+++ Exercicios/ex002.go
@@ -1,6 +1,5 @@
 //  Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
 
-
 package main
 
 import "fmt"
---
+++ Exercicios/ex003.go
@@ -0,0 +1,18 @@
+// Faça um Programa que peça dois números e imprima a soma. 
+
+package main
+
+import "fmt"
+
+func main(){
+	x := 1438
+	y := 576
+	soma := x + y
+
+	fmt.Printf("A soma entre %d e %d é: %d",x,y,soma)
+
+}
+
+
+
+

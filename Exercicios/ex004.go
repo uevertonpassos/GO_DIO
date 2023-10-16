a637f468f58f609244680d1a946f6702a0e0c7fd
Ueverton Passos <uevertonpassos96@gmail.com>
2023-10-16 19:36:49 -0300

    finalizando por hj

---
+++ Exercicios/ex004.go
@@ -0,0 +1,14 @@
+// Peça ao usuário para inserir uma série de números e, em seguida, calcule e exiba a média desses números
+
+package main
+
+import "fmt"
+
+func main(){
+	num1 := 8
+	num2 := 7
+	num3 := 10
+	num4 := 6
+	media := (num1 + num2 + num3 + num4)/4
+	fmt.Printf("A media desses valores será:%d", media)
+}

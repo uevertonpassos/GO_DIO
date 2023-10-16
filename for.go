bdbbe9e5a662d48f0373f84bda8c13bdd21cf811
Ueverton Passos <uevertonpassos96@gmail.com>
2023-10-16 17:41:00 -0300

    estruturas de controle em GO, inicio

--- for.go
+++ for.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main(){
+
+	//for e IF em go
 	for i := 1; i<=10; i++{
-		fmt.Println(i)
+
+		if i%2 == 0{
+			fmt.Println("par")
+		}else {
+			fmt.Println("ímpar!")
+		}
 	}
 }

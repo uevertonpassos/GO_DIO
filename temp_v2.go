7cb74f1b005401371379fc9e93b1dfbf167298a1
Ueverton Passos <uevertonpassos96@gmail.com>
2023-10-16 17:17:19 -0300

    treinando for

--- temp_v2.go
+++ temp_v2.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-const ebulicaoF  = 212.0
+const ebulicaoF = 212.0
 
 func main(){
-	tempF  := ebulicaoF
-	tempC  := (tempF - 32)*5/9 
+	tempF := ebulicaoF
+	tempC := (tempF - 32)*5/9 
 
 	fmt.Printf("A ebulição da água acontecem em %.2f °F e em %.2f em °C", tempF,
 	tempC)

87cefd97600af771c7ef3b5584079c0d41ef0a63
Ueverton Passos <uevertonpassos96@gmail.com>
2023-10-16 19:06:33 -0300

    exercicios basicos

--- Exercicios/ex001.go
+++ Exercicios/ex001.go
@@ -1,7 +1,8 @@
 package main
 
 import "fmt"
-
+ 
+//Faça um Programa que mostre a mensagem "Alo mundo" na tela. 
 func main(){
 	fmt.Println("Hello, world!")
 }
---
+++ Exercicios/ex002.go
@@ -0,0 +1,11 @@
+//  Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+
+
+package main
+
+import "fmt"
+
+func main(){
+	num := 2
+
+	fmt.Printf("o número digitado foi:%d",num)}

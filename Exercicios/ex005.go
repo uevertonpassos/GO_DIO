3701cbc1ff2beb9538abd939599c66b2812e6565
Ueverton Passos <uevertonpassos96@gmail.com>
2023-10-18 21:38:22 -0300

    teste

--- Exercicios/ex005.go
+++ Exercicios/ex005.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main(){
 
-	for i := 0;i<=10;i++{
-		if i > 5{
-			fmt.Println(i)
+	for i := 0;i<=10;i++{ //prestar atenção na declaração
+		if i > 5 || i< 8{
+			fmt.Println(i, "LANA")
 		}else{
 			fmt.Println("aqui é:", i)
 		}

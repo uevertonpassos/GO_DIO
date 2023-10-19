2c15305ce4008af811e17547fa134a4ae647c30f
Ueverton Passos <uevertonpassos96@gmail.com>
2023-10-19 17:54:04 -0300

    não me pergunte como isso funcionou

--- Exercicios/ex010.go
+++ Exercicios/ex010.go
@@ -8,7 +8,7 @@ import "fmt"
 func main(){
 	n:= 10 //numero de termos da sequência
 	a, b := 0,1
-
+ 
 	for i:=0; i< n; i++{
 		fmt.Println(a)
 		a, b = b, a+b
---
+++ Exercicios/ex011.go
@@ -0,0 +1,15 @@
+//Calcule a média de 10 números.
+
+package main
+
+import "fmt"
+
+func main(){
+	numeros := 0.0
+
+	for i:=0.0; i<=10.0; i++{
+		numeros = numeros +i
+
+	}
+	fmt.Println(numeros/10.0)
+}

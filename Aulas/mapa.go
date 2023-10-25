6431f712dc559cecc531a6441046be5f26ed4d10
Ueverton Passos <uevertonpassos96@gmail.com>
2023-10-25 15:32:13 -0300

    maps

---
+++ Aulas/mapa.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	/*x:= make(map[string]int)
+	x["chave"] = 10
+	fmt.Println(x["chave"])*/
+	elemento:= make(map[string]string)
+	elemento["H"] = "Hidrogênio"
+	elemento["He"] = "Helio"
+	elemento["Ne"] = "Neon"
+	fmt.Println(elemento["Ne"])
+}
+
